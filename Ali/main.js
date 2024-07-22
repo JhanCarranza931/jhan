12a24564b3c27eba937a8298e409f8888f22114b
JhanCarranza931 <130426201+JhanCarranza931@users.noreply.github.com>
2024-07-22 00:06:02 -0500

    Add files via upload

---
+++ Ali/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $(function () {
+    var flame = $("#flame");
+    var txt = $("h1");
+
+    flame.on({
+        click: function () {
+        flame.removeClass("burn").addClass("puff");
+        $(".smoke").each(function () {
+            $(this).addClass("puff-bubble");
+        });
+        $("#glow").remove();
+        txt.hide().html("🥳Happy birthay Ali <br> Eres la mejor. <br> Te Quiero❤️").delay(750).fadeIn(300);
+        $("#candle").animate(
+            {
+            opacity: ".5"
+            },
+            100
+        );
+        }
+    });
+});});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("flame").addEventListener("click", function() {
+        var audio = document.getElementById("backgroundSound");
+        audio.play().catch(function(error) {
+            console.log("Audio playback failed: " + error);
+        });
+    });
+});
